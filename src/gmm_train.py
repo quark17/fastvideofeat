0478a305634bcdcd66feac9bd6346edb749b804a
Julie Schwartz <quark@bluespec.com>
2026-05-12 17:47:41 +1200

    gmm_train: Add command-line arg for number of threads

--- src/gmm_train.py
+++ src/gmm_train.py
@@ -8,6 +8,7 @@ import yael
 parser = argparse.ArgumentParser()
 parser.add_argument('--gmm_ncomponents', type = int, required = True)
 parser.add_argument('--vocab', nargs = 2, required = True)
+parser.add_argument('--nthreads', type = int, required = False)
 args = parser.parse_args()
 cutFrom, cutTo = map(int, args.vocab[0].split('-'))
 
@@ -15,10 +16,15 @@ data = np.loadtxt(sys.stdin, dtype = np.float32, usecols = range(cutFrom, 1 + cu
 
 npoints, nfeatures = data.shape
 niter = 50
-nthreads = 1
+nthreads = args.nthreads
 seed = 0
 redo = 1
 flags = yael.GMM_FLAGS_W
+
+if ((nthreads < 1) or (nthreads > 16)):
+    print('Bad nthreads value: %d' % nthreads)
+    raise SystemExit(1)
+
 gmm = yael.gmm_learn(nfeatures, npoints, args.gmm_ncomponents, niter, yael.FloatArray.acquirepointer(yael.numpy_to_fvec(data)), nthreads, seed, redo, flags)
 
 yael.gmm_write(gmm, args.vocab[1])
