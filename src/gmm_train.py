e0871dc30acafc259377103604a8dab55c9cd502
Julie Schwartz <quark@bluespec.com>
2026-05-13 23:16:40 +1200

    gmm_train: Add command-line arg for the seed

--- src/gmm_train.py
+++ src/gmm_train.py
@@ -8,7 +8,8 @@ import yael
 parser = argparse.ArgumentParser()
 parser.add_argument('--gmm_ncomponents', type = int, required = True)
 parser.add_argument('--vocab', nargs = 2, required = True)
-parser.add_argument('--nthreads', type = int, required = False)
+parser.add_argument('--seed', type = int, required = False, default = 0)
+parser.add_argument('--nthreads', type = int, required = False, default = 1)
 args = parser.parse_args()
 cutFrom, cutTo = map(int, args.vocab[0].split('-'))
 
