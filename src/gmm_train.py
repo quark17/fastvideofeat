65314a66170bcd3567a04e11b302bf4b73d9ca50
Julie Schwartz <quark@bluespec.com>
2026-05-19 12:48:32 +1200

    gmm_train: Add flag for setting the number of iterations

--- src/gmm_train.py
+++ src/gmm_train.py
@@ -11,13 +11,14 @@ parser.add_argument('--vocab', nargs = 2, required = True)
 parser.add_argument('--seed', type = int, required = False, default = 0)
 parser.add_argument('--redo', type = int, required = False, default = 1)
 parser.add_argument('--nthreads', type = int, required = False, default = 1)
+parser.add_argument('--niter', type = int, required = False, default = 50)
 args = parser.parse_args()
 cutFrom, cutTo = map(int, args.vocab[0].split('-'))
 
 data = np.loadtxt(sys.stdin, dtype = np.float32, usecols = range(cutFrom, 1 + cutTo))
 
 npoints, nfeatures = data.shape
-niter = 50
+niter = args.niter
 nthreads = args.nthreads
 seed = args.seed
 redo = args.redo
